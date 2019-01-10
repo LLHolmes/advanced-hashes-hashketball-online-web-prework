0ccfbd6ef0a1dd25bebfbf5e28faf972b4d810a7
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 16:00:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,7 +156,7 @@ end
 def team_colors(team)
   game_hash.each do |location, team_data|
     team_data.each do |team_attribute, data|
-      if team_attribute[:team_name] == :players
+      if team_attribute == :players
         data.each do |team_member, stats|
           if team_member == player
             stats.each do |list, value|
